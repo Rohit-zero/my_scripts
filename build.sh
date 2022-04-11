5f7bc58c2da819b3c22d51ed271329c64321564d
Rohit39 <77542893+Rohit-zero@users.noreply.github.com>
2022-04-11 14:27:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Cloning dependencies"
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable  clang
 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
 git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
-git clone --depth=1 https://github.com/Rohit-zero/AnyKernel.git
+git clone --depth=1 https://github.com/Rohit-zero/AnyKernel3.git
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
